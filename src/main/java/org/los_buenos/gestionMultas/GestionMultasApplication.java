0c07b4e7de6384942af47aa83f26009abbca268b
BradOlv <boliva-2023502@kinal.edu.gt>
2025-09-12 22:17:01 -0600

    T3 - commit principal de la creación del proyecto

---
+++ src/main/java/org/los_buenos/gestionMultas/GestionMultasApplication.java
@@ -0,0 +1,13 @@
+package org.los_buenos.gestionMultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMultasApplication.class, args);
+	}
+
+}
