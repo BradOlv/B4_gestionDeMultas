673dcfd3c1400c586332635baaed447e57398bf5
BradOlv <boliva-2023502@kinal.edu.gt>
2025-09-16 08:39:51 -0600

    Arreglados unos cambios de llaves foraneas

--- src/main/java/org/los_buenos/gestionMultas/dominio/service/DepartamentosService.java
+++ src/main/java/org/los_buenos/gestionMultas/dominio/service/DepartamentosService.java
@@ -3,17 +3,22 @@ package org.los_buenos.gestionMultas.dominio.service;
 import lombok.Data;
 import org.los_buenos.gestionMultas.dominio.dto.DepartamentosDto;
 import org.los_buenos.gestionMultas.dominio.repository.DepartamentosRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 @Data
 public class DepartamentosService {
-    private DepartamentosRepository departamentosRepository;
-    public List<DepartamentosDto> obtenerTodo(){return this.departamentosRepository.obtenerTodo();}
-    public DepartamentosDto buscarPorId(Integer id){return this.departamentosRepository.buscarPorId(id);}
-    public DepartamentosDto guardar(DepartamentosDto departamentosDto){return this.departamentosRepository.guardar(departamentosDto);}
-    public void eliminar(Integer id){this.departamentosRepository.eliminar(id);}
+
+    private final DepartamentosRepository departamentosRepository;
+    public DepartamentosService(DepartamentosRepository departamentosRepository) {this.departamentosRepository = departamentosRepository;
+    }
+    public List<DepartamentosDto> obtenerTodo(){return this.departamentosRepository.obtenerTodo();
+    }
+    public DepartamentosDto buscarPorId(Integer id){return this.departamentosRepository.buscarPorId(id);
+    }
+    public DepartamentosDto guardar(DepartamentosDto departamentosDto){return this.departamentosRepository.guardar(departamentosDto);
+    }
+    public void eliminar(Integer id){this.departamentosRepository.eliminar(id);
+    }
 }
