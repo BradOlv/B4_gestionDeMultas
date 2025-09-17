cb57e4057b9ea9f267ddc539ea2c9563ec85a5b0
breynerbd <bbenitez-2021497@kinal.edu.gt>
2025-09-17 11:29:17 -0600

    Implementacion de archivo data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into Vehiculos(marca, matricula, modelo)
+values("Honda", "P598HTJ", "Civic");
+
+insert into Inspectores(nombre_inspector, numero_placa)
+values("Jose Luis", "H-9864");
+
+insert into Infractores(direccion, dni, email, nombre_infractor)
+values("Cayala", "1", "bb@gmail.com", "Eduardo Salazar");
+
+insert into Departamentos(direccion_oficinas, municipio, nombre_departamento)
+values("El Naranjo", "Guatemala", "PNC");
