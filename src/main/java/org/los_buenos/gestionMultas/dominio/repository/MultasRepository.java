0a8c70914f2980dab9ac6d6da7b6b3f36ae82a88
Informatica <informatica@alumno.local>
2025-09-24 10:42:59 -0600

    T48 - realizar pruebas de tabla Vehículos

--- src/main/java/org/los_buenos/gestionMultas/dominio/repository/MultasRepository.java
+++ src/main/java/org/los_buenos/gestionMultas/dominio/repository/MultasRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface MultasRepository {
     List<MultasDto> obtenerTodo();
     MultasDto buscarPorId(Integer id);
+    List<MultasDto> buscarPorIdVehiculo(Integer idVehiculo);
+    List<MultasDto> buscarPorIdInfractor(Integer idInfractor);
     MultasDto guardar(MultasDto multa);
     void eliminar(Integer id);
 }
