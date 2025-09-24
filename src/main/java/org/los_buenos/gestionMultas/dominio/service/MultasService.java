14da789a3df9af7e64eb66f5582b88c8f3d4ac78
breynerbd <bbenitez-2021497@kinal.edu.gt>
2025-09-23 18:37:34 -0600

    T35 - Manejo de controlador para vista Multas

--- src/main/java/org/los_buenos/gestionMultas/dominio/service/MultasService.java
+++ src/main/java/org/los_buenos/gestionMultas/dominio/service/MultasService.java
@@ -1,8 +1,8 @@
 package org.los_buenos.gestionMultas.dominio.service;
 
 import lombok.Data;
-import org.los_buenos.gestionMultas.dominio.dto.MultasDto;
-import org.los_buenos.gestionMultas.dominio.repository.MultasRepository;
+import org.los_buenos.gestionMultas.dominio.dto.*;
+import org.los_buenos.gestionMultas.dominio.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +12,18 @@ import java.util.List;
 @Data
 public class MultasService {
     private final MultasRepository multasRepository;
+    private final InfractorRepository infractorRepository;
+    private final InspectoresRepository inspectorRepository;
+    private final DepartamentosRepository departamentoRepository;
+    private final VehiculoRepository vehiculoRepository;
+
     public List<MultasDto>  obtenerTodo() { return this.multasRepository.obtenerTodo();}
     public MultasDto buscarPorId(Integer id) { return this.multasRepository.buscarPorId(id);}
     public MultasDto guardar(MultasDto multa) { return this.multasRepository.guardar(multa);}
     public void eliminar(Integer id) {this.multasRepository.eliminar(id);}
+
+    public List<InfractorDto> obtenerInfractores() { return infractorRepository.obtenerTodo(); }
+    public List<InspectoresDto> obtenerInspectores() { return inspectorRepository.obtenerTodo(); }
+    public List<DepartamentosDto> obtenerDepartamentos() { return departamentoRepository.obtenerTodo(); }
+    public List<VehiculoDto> obtenerVehiculos() { return vehiculoRepository.obtenerTodo(); }
 }
